c4230cd600b1f6054de7f8cfd13038bc11f92cea
GitJournal <app@gitjournal.io>
2022-10-09 18:01:50 +0800

    [2022-10-09 10:01:50]
    FILE:
    lazero/filesystem/io.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/filesystem/io.py
+++ lazero/filesystem/io.py
@@ -19,6 +19,7 @@ def writeFileBinary(filename, content, mode="wb+"):
 
 import pickle
 import dill
+from typing import Literal
 backends = {'pickle': pickle, 'dill': dill}
 def readPythonObjectFromFile(filename, backend=''):
     data = readFileBinary(filename)
