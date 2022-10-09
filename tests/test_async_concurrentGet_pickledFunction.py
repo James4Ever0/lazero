647bab3499e6e2290a433f1f12467c97632f1da4
GitJournal <app@gitjournal.io>
2022-10-09 13:09:32 +0800

    [2022-10-09 05:09:32]
    FILE:
    tests/test_async_concurrentGet_pickledFunction.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_async_concurrentGet_pickledFunction.py
+++ tests/test_async_concurrentGet_pickledFunction.py
@@ -5,7 +5,7 @@ from lazero.network.asyncio import concurrentGet
 url = "http://127.0.0.1:8932"
 urlList = [url]*10
 
-result = concurrentGet(urlList)
+result = concurrentGet(urlList, processor = lambda x: dir(x))
 print(result)
 # object ClientResponse can't be used in 'await' expression
 # so what?
