f8830bf0fcc8c38a836829c964e4a9787484d367
GitJournal <app@gitjournal.io>
2022-10-09 17:58:29 +0800

    [2022-10-09 09:58:29]
    FILE:
    lazero/filesystem/io.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/filesystem/io.py
+++ lazero/filesystem/io.py
@@ -1,9 +1,9 @@
 def readFile(filename, encoding='utf-8'):
-    with open(filename, encoding=encoding) as f:
+    with open(filename, 'r+encoding=encoding) as f:
         return f.read()
 
 def writeFile(filename, content, encoding='utf-8'):
-    with open(filename, encoding=encoding) as f:
+    with open(filename, 'r+encoding=encoding) as f:
         f.write(content)
 
 def readFileBinary(filename, encoding='utf-8'):
