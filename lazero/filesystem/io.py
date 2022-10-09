1030cda4d2b8470e1aca3fbc6858101ad30efba4
GitJournal <app@gitjournal.io>
2022-10-09 18:00:35 +0800

    [2022-10-09 10:00:35]
    FILE:
    lazero/filesystem/io.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/filesystem/io.py
+++ lazero/filesystem/io.py
@@ -1,15 +1,21 @@
-def readFile(filename, encoding='utf-8', mode='r+'):
-    with open(filename, mode,encoding=encoding) as f:
+def readFile(filename, encoding="utf-8", mode="r+"):
+    with open(filename, mode, encoding=encoding) as f:
         return f.read()
 
-def writeFile(filename, content, encoding='utf-8', mode='w+'):
-    with open(filename, mode,encoding=encoding) as f:
+
+def writeFile(filename, content, encoding="utf-8", mode="w+"):
+    with open(filename, mode, encoding=encoding) as f:
         f.write(content)
 
-def readFileBinary(filename, mode='rb'):
-    with open(filename,mode) as f:
+
+def readFileBinary(filename, mode="rb"):
+    with open(filename, mode) as f:
         return f.read()
 
-def writeFileBinary(filename, content, mode= 'wb+'):
-    with open(filename,mode) as f:
+
+def writeFileBinary(filename, content, mode="wb+"):
+    with open(filename, mode) as f:
         f.write(content)
+
+import pickle
+import dill
