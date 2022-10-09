3d68fa35796e2e03d92c4f003dff212b77f35655
GitJournal <app@gitjournal.io>
2022-10-09 23:55:48 +0800

    [2022-10-09 15:55:48]
    FILE:
    build/lib/lazero/filesystem/temp.py
    lazero.egg-info/SOURCES.txt
    build/lib/lazero/filesystem/io.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ build/lib/lazero/filesystem/io.py
@@ -0,0 +1,37 @@
+def readFile(filename, encoding="utf-8", mode="r"):
+    with open(filename, mode, encoding=encoding) as f:
+        return f.read()
+
+
+def writeFile(filename, content, encoding="utf-8", mode="w+"):
+    with open(filename, mode, encoding=encoding) as f:
+        f.write(content)
+
+
+def readFileBinary(filename, mode="rb"):
+    with open(filename, mode) as f:
+        return f.read()
+
+
+def writeFileBinary(filename, content, mode="wb"):
+    with open(filename, mode) as f:
+        f.write(content)
+
+
+import pickle
+import dill
+from typing import Literal
+
+backends = {"pickle": pickle, "dill": dill}
+
+
+def readPythonObjectFromFile(filename, backend: Literal["pickle", "dill"] = "dill"):
+    data = readFileBinary(filename)
+    return backends[backend].loads(data)
+
+
+def writePythonObjectToFile(
+    filename, pythonObject, backend: Literal["pickle", "dill"] = "dill"
+):
+    data = backends[backend].dumps(pythonObject)
+    writeFileBinary(filename, data)
