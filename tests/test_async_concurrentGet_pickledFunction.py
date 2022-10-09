d0284ad4d57677dfa02519739b0c9236ddd9a9fd
GitJournal <app@gitjournal.io>
2022-10-09 13:14:19 +0800

    [2022-10-09 05:14:19]
    FILE:
    tests/test_async_concurrentGet_pickledFunction.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_async_concurrentGet_pickledFunction.py
+++ tests/test_async_concurrentGet_pickledFunction.py
@@ -6,6 +6,7 @@ url = "http://127.0.0.1:8932"
 urlList = [url]*10
 
 # result = concurrentGet(urlList) # unable to load the raw thing. we can see that.
+# at least we can have the type, dir.
 result = concurrentGet(urlList, processor = lambda x: type(x))
 # result = concurrentGet(urlList, processor = lambda x: dir(x))
 print(result)
