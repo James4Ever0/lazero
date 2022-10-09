8c03b3e7d3045f65dc676d3b5d107d687f19ec4c
GitJournal <app@gitjournal.io>
2022-10-10 01:51:44 +0800

    [2022-10-09 17:51:44]
    FILE:
    lazero/program/functools.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/program/functools.py
+++ lazero/program/functools.py
@@ -281,3 +281,29 @@ def skipExceptionDebug(func):
 
 
 # breakpoint()
+
+def iterateWithTempDirectory(tempdir: Union[str, None] = None):
+    # iterate is some added keyword.
+    if tempdir is None:
+        contextManager = nullcontext()
+    else:
+        contextManager = tmpdir(tempdir)
+
+    def inner(func):
+        def wrapper(
+            generatorMaybe, iterate=False, **kwargs
+        ):  # this wrapper will void function input signatures maybe? anyway let's do it!
+            def iterator(generatorMaybe, **kwargs):
+                with contextManager:
+                    for elem in generatorMaybe:
+                        yield func(elem, **kwargs)
+
+            if iterate:
+                return iterator(generatorMaybe, **kwargs)
+            else:
+                with contextManager:
+                    return func(generatorMaybe, **kwargs)
+
+        return wrapper
+
+    return inner
