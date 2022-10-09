96082153bb8bc971c7c9421197efa2fd8fcc78dd
GitJournal <app@gitjournal.io>
2022-10-10 02:24:43 +0800

    [2022-10-09 18:24:42]
    FILE:
    
    DIR:
    build/
    lazero.egg-info/
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

---
+++ build/lib/lazero/filesystem/__init__.py
@@ -0,0 +1,1 @@
+from lazero.filesystem.temp import *
---
+++ build/lib/lazero/network/progressbar/server.py
@@ -0,0 +1,58 @@
+# try to update progressbar via network.
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from tqdm import tqdm
+
+t = None
+
+@app.get('/')
+def hello():
+    return 'progressbar server'
+
+# not routing this to network.
+def close_progressbar():
+    global t
+    if t is not None:
+        try:
+            t.close()
+            return {'msg':'success'}
+        except:
+            import traceback
+            traceback.print_exc()
+            print('error closing progressbar')
+            return {'msg':'error closing progressbar'}
+
+@app.get('/reset')
+def reset(total: int, name:str='random task'): # pass the iteration count
+    global t
+    close_progressbar()
+    print('processing:', name)
+    t = tqdm(total=total)
+    return {'msg':'success'}
+
+@app.get('/update')
+def update_progressbar(progress: int=1, info:str=""):
+    global t
+    if info !="":
+        print('update info:', info)
+    if t is not None:
+        try:
+            t.clear()
+            t.update(progress)
+            return {'msg':'success'}
+        except:
+            import traceback
+            traceback.print_exc()
+            print("error when updating progessbar")
+            return {'msg':'error when updating progessbar'}
+    else:
+        print('no progressbar available')
+        return {'msg':'no progressbar available'}
+
+
+@app.get('/close')
+def close():
+    close_progressbar()
+    return {'msg':'success'}
