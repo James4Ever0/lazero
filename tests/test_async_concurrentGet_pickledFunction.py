9fb48b16d48024b35a5e0b022cb6e0d74851cd85
GitJournal <app@gitjournal.io>
2022-10-09 13:14:02 +0800

    [2022-10-09 05:14:02]
    FILE:
    tests/test_async_concurrentGet_pickledFunction.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- tests/test_async_concurrentGet_pickledFunction.py
+++ tests/test_async_concurrentGet_pickledFunction.py
@@ -6,6 +6,7 @@ url = "http://127.0.0.1:8932"
 urlList = [url]*10
 
 # result = concurrentGet(urlList) # unable to load the raw thing. we can see that.
+result = concurrentGet(urlList, processor = lambda x: type(x))
 # result = concurrentGet(urlList, processor = lambda x: dir(x))
 print(result)
 # object ClientResponse can't be used in 'await' expression
