e03226c8bd1358a54d3a79e323d6dd193c76b225
GitJournal <app@gitjournal.io>
2022-11-11 01:58:49 +0800

    [2022-11-10 17:58:49]
    FILE:
    lazero/utils/json.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/utils/json.py
+++ lazero/utils/json.py
@@ -115,5 +115,5 @@ def jsonDeleteAllInstances(jsonObj, isInstance: typing.Callable[[typing.Any], bo
 def jsonify(jsonObj):  # remove ellipsis
     jsonObj2=json.loads(json.dumps(jsonObj))
     isInstance = lambda obj: obj == ...
-    jsonObj2 = jsonDeleteAllInstances(jsonObj, isInstance)
-    return json.loads(json.dumps(jsonObj2))
+    jsonObj2 = jsonDeleteAllInstances(jsonObj2, isInstance)
+    return json.loads(json.dumps(jsonObj2
