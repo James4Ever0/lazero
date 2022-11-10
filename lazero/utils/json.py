d7fb38f22fb89254a9dea887b5f682c68fb885ee
GitJournal <app@gitjournal.io>
2022-11-11 01:31:19 +0800

    [2022-11-10 17:31:19]
    FILE:
    lazero/utils/json.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/utils/json.py
+++ lazero/utils/json.py
@@ -90,6 +90,6 @@ def jsonify(jsonObj): # remove ellipsis
             candidates.append(key)
     candidates.sort(key=lambda x: -x[-1] if type(x[-1]) == int else 1)
     for candidate in candidates:
-
-        obj = jsonObj2.get()
+        obj = jsonObj2
+        for key in candidate[].get()
     return json.loads(json.dumps(jsonObj2))
