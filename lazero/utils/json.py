c7d4eaa1f9d2a2533189e1cad23b6a4995fd4491
GitJournal <app@gitjournal.io>
2022-11-11 01:24:28 +0800

    [2022-11-10 17:24:28]
    FILE:
    lazero/utils/json.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/utils/json.py
+++ lazero/utils/json.py
@@ -85,4 +85,5 @@ def jsonify(jsonObj): # remove ellipsis
     jsonObj2 = jsonObj.copy()
     for key, value in jsonWalk(jsonObj2):
         if value == ...:
+            # delete that thing!
     return json.loads(json.dumps(jsonObj2))
