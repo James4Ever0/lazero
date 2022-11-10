f2342b855536e5b368918eccd6783676c48cdb22
GitJournal <app@gitjournal.io>
2022-11-11 01:33:05 +0800

    [2022-11-10 17:33:05]
    FILE:
    lazero/utils/json.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/utils/json.py
+++ lazero/utils/json.py
@@ -79,6 +79,7 @@ def jsonUpdate(jsonObj, location=[], update_content=None):
             raise Exception("Unsupported JSON update target type:", type(jsonObj))
     return update_content
 
+def jsonDeleteObject(jsonObj,)
 
 @reloading
 def jsonify(jsonObj): # remove ellipsis
