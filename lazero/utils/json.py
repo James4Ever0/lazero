53195faa7565b3a2d73b246a40f1da745e751dcb
GitJournal <app@gitjournal.io>
2022-11-11 01:50:36 +0800

    [2022-11-10 17:50:36]
    FILE:
    lazero/utils/json.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/utils/json.py
+++ lazero/utils/json.py
@@ -92,7 +92,7 @@ def jsonDeleteObject(jsonObj, location: list):
 # how to reload module directly, so we can include this function as well?
 import typing
 @reloading
-def jsonDeleteAllInstances(jsonObj, isInstance:typin):
+def jsonDeleteAllInstances(jsonObj, isInstance:typing.Callable[]):
     jsonObj2 = jsonObj.copy()
     candidates = []
     for key, value in jsonWalk(jsonObj2):
