429814d6e997e9bfeef7a6f969bd96aeb3def3c9
GitJournal <app@gitjournal.io>
2022-11-11 01:29:27 +0800

    [2022-11-10 17:29:27]
    FILE:
    lazero/utils/json.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/utils/json.py
+++ lazero/utils/json.py
@@ -88,5 +88,5 @@ def jsonify(jsonObj): # remove ellipsis
         if value == ...:
             # delete that thing! but how to delete these things once for all?
             candidates.append(key)
-    candidates.sort(key=lambda x: x[-1])
+    candidates.sort(key=lambda x: x[-1] if type(x) == int else )
     return json.loads(json.dumps(jsonObj2))
