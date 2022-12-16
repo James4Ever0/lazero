9b001da2a678147f82db489b9e2418bb68130830
GitJournal <app@gitjournal.io>
2022-12-17 05:27:16 +0800

    [2022-12-16 21:27:16]
    FILE:
    lazero/network/checker.py
    
    DIR:
    
    SYMLINK:
    
    DELETE:
    
    IGNORE DIR:
    
    IGNORE FILE GLOB:
    
    IGNORE FILEPATH:

--- lazero/network/checker.py
+++ lazero/network/checker.py
@@ -6,7 +6,7 @@ def waitForServerUp(port, message, timeout=1, maxtime=-1):
         try:
             if mflag:
                 mtime -= 1
-                print("")
+                print("{} chances remains for server {} at {}")
             url = "http://localhost:{}".format(port)
             with requests.get(url, timeout=timeout, proxies=None) as r:
                 if type(message) == str:
